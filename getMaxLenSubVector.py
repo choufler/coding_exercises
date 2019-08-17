0f2bf60682e82caa085ac60d87b50cff00ec9f34
choufler <choufler@gail.com>
2019-08-17 16:02:11 +0100

    last changes

--- getMaxLenSubVector.py
+++ getMaxLenSubVector.py
@@ -1,18 +1,20 @@
 def getMaxAreaSubVector(vector, S):
     WindowMaxLen = 0
-    windowSum = 0
     WindowStart = 0
+
+    currrentSum = 0    
     currentRangeStart = 0
     for i in range (0, len(vector)):
-        windowSum += vector[i]
-        if windowSum <= S:            
+        currrentSum += vector[i]
+        if currrentSum <= S:            
             WindowMaxLen +=1
             WindowStart = currentRangeStart
         else:
-            windowSum-= vector[currentRangeStart]
+            currrentSum-= vector[currentRangeStart]
             currentRangeStart+=1
     return vector[WindowStart:WindowStart + WindowMaxLen]
             
+vec = [1,2,7,100,4,8,3,1,1,1,1,1,2,3,4,5,6]
+maxSum = 5
+print( getMaxAreaSubVector(vec, maxSum) )
 
-res = getMaxAreaSubVector([1,2,7,100,4,8,3,1,1,1,1,1,2,3,4,5,6], 5)
-print(res)
